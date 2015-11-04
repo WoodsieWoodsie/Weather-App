49418bc70ca6c8bf4a6eb91911288315bcfd6c3e
Rachel Wood <r@codinghouse.co>
2015-11-03 20:00:29 -0800

    Allow finding current conditions for specific ZIP code with drawing to DOM

--- main.js
+++ main.js
@@ -19,13 +19,13 @@ function init() {
         console.log("user zip conditions", data.current_observation)
         var current = data.current_observation;
         var icon = current.icon_url;
-        console.log("icon", icon);
+        var $userLocation = $('<p>').text(current.display_location.full);
         var $userCurrentIcon = $('<img>').attr('src', icon);
         var $weather = $('<p>').text(current.weather);
         var $temp_f = $('<p>').text(current.temp_f);
         var $feelslike_f = $('<p>').text(current.feelslike_f);
         var $observation_time = $('<p>').text(current.observation_time);
-        $('#userCurrent').append($userCurrentIcon, $weather, $temp_f, $feelslike_f, $observation_time);
+        $('#userCurrent').append($userCurrentIcon, $userLocation, $weather, $temp_f, $feelslike_f, $observation_time);
       })
       .fail(function(error){
         console.error(error);
@@ -46,16 +46,23 @@ function init() {
 }
 
 function getWeather() {
-
+  // GET CURRENT CONDITIONS FOR ZIP INPUT
   var $zip = $('#zipInput').val()
   console.log("zip code", $zip)
-
-  var zipUrl = apiUrl + 'forecast/q/' + $zip + '.json'
+  var zipUrl = apiUrl + 'conditions/q/' + $zip + '.json';
   console.log("zip url", zipUrl);
-  
   $.get(zipUrl)
   .done(function(data){
-    console.log("data", data.forecast);
+    console.log("data", data);
+    var zipCurrent = data.current_observation;
+    var $zipLocation = $('<p>').text(zipCurrent.display_location.full);
+    var $zipIcon = $('<img>').attr('src', zipCurrent.icon_url);
+    var $zipWeather = $('<p>').text(zipCurrent.weather);
+    var $zipTemp_f = $('<p>').text(zipCurrent.temp_f);
+    var $zipFeelslike_f = $('<p>').text(zipCurrent.feelslike_f);
+    var $zipObservation_time = $('<p>').text(zipCurrent.observation_time);
+    $('#userCurrent').empty().append($zipIcon, $zipLocation, $zipWeather, $zipTemp_f, $zipFeelslike_f, $zipObservation_time);
+
   })
   .fail(function(error){
     console.error(error);
